30e79b6c0ed32efc2ea779ea86c9fa1c2467676c
artem <alatih@mail.ru>
2018-03-16 02:59:07 +0300

    with new task

---
+++ secondtask/target/generated-sources/annotations/com/train/newtask/entity/Station_.java
@@ -0,0 +1,15 @@
+package com.train.newtask.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Station.class)
+public abstract class Station_ {
+
+	public static volatile SingularAttribute<Station, String> station_name;
+	public static volatile SingularAttribute<Station, Integer> station_id;
+
+}
+
---
+++ secondtask/target/generated-sources/annotations/com/train/newtask/entity/Train_.java
@@ -0,0 +1,16 @@
+package com.train.newtask.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Train.class)
+public abstract class Train_ {
+
+	public static volatile SingularAttribute<Train, Integer> places;
+	public static volatile SingularAttribute<Train, String> train_number;
+	public static volatile SingularAttribute<Train, Integer> train_id;
+
+}
+
